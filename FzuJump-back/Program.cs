915bf341d8bb4ac6e33bb9f27fb0593a77ef0750
daisyyyyyyyyyy <103107145+daisyyyyyyyyyy@users.noreply.github.com>
2022-11-18 01:14:17 +0800

    关于蓄力条上限的代码修改

--- FzuJump-back/Program.cs
+++ FzuJump-back/Program.cs
@@ -3,16 +3,16 @@ using static System.Runtime.CompilerServices.RuntimeHelpers;
 
 public class PlayerController : MonoBehaviour//在PlayerController类中
 {
-    public bool IsDoublePressForce = false;//新增有关pressForce道具效果的判断位
-    void ScrollbarMaS()//在ScrollbarMas方法中
+    void ScrollbarMaS()//在该类中
     {
-        if (Input.GetKeyUp(KeyCode.W))//在该条件下
+        if (Input.GetKey(KeyCode.W))//在该条件下
         {
-            if (IsDoublePressForce)//增加道具使用过后失效的语句
+            if (xulitiao.size < 1)//增加对蓄力条是否已满的判断
             {
-                IsDoublePressForce = false;
-                pressForce /= 2;
+                presstime += Time.deltaTime;
+                xulitiao.size = presstime / 2f;
             }
+
         }
     }
 }
