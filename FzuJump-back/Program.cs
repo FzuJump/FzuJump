2cb263b26b3911f7fc8bd9b0b152b4be0b3b215c
daisyyyyyyyyyy <103107145+daisyyyyyyyyyy@users.noreply.github.com>
2022-11-18 01:24:01 +0800

    pressForce翻倍道具在PlayerController类（jump）中增加的代码部分

--- FzuJump-back/Program.cs
+++ FzuJump-back/Program.cs
@@ -3,16 +3,16 @@ using static System.Runtime.CompilerServices.RuntimeHelpers;
 
 public class PlayerController : MonoBehaviour//在PlayerController类中
 {
-    void ScrollbarMaS()//在该类中
+    public bool IsDoublePressForce = false;//新增对pressForce翻倍道具的判断位
+    void ScrollbarMaS()//在该方法
     {
-        if (Input.GetKey(KeyCode.W))//在该条件下
+        if (Input.GetKeyUp(KeyCode.W))//在该条件
         {
-            if (xulitiao.size < 1)//增加对蓄力条是否已满的判断
+            if (IsDoublePressForce)//新增对pressForce翻倍道具失效的语句
             {
-                presstime += Time.deltaTime;
-                xulitiao.size = presstime / 2f;
+                IsDoublePressForce = false;
+                pressForce /= 2;
             }
-
         }
     }
 }
