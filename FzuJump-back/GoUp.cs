d1fa3b3bc3def058c8875f3041dd6b0201c5802c
daisyyyyyyyyyy <103107145+daisyyyyyyyyyy@users.noreply.github.com>
2022-11-20 14:42:57 +0800

    垂直上升一段距离的道具类

--- FzuJump-back/GoUp.cs
+++ FzuJump-back/GoUp.cs
@@ -1,28 +1,18 @@
-﻿using System.Numerics;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
 
-public class PlayerController : MonoBehaviour//在该类
+public class GoUp : MonoBehaviour
 {
-    //新增以下变量
-    public bool Is_Go_Up = false;
-    public float Go_Up_Speed = 8f;
-    public float Go_Up_Time = 0f;
-    public float Go_Up_Distance = 20f;
-    void Update()
+    public GameObject PlayerController;
+    
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        GoUp();//新增该调用语句
-    }
-    void GoUp()//实现垂直上升一段距离
-    {
-        if (Is_Go_Up)
+        if (collision.tag == "Player")
         {
-            Go_Up_Time += Time.deltaTime;
-            rb.velocity = new Vector2(0, Go_Up_Speed);
-        }
-
-        if (Go_Up_Time * Go_Up_Speed >= Go_Up_Distance)
-        {
-            Is_Go_Up = false;
-            Go_Up_Time = 0f;
+            PlayerController.GetComponent<PlayerController>(). Is_Go_Up = true;
+            
         }
     }
 }
