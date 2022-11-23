6baf47c50b56bf0a4204ce8e871e37b507acf903
Bofishll <112791527+Bofishll@users.noreply.github.com>
2022-11-23 20:26:18 +0800

    增加生命道具

---
+++ Unity/Resurrection.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resurrection : MonoBehaviour
+{
+    public GameObject Player;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Player.GetComponent<PlayerController>().Life ++;//ÉúÃüÊý++
+        }
+    }
+}
